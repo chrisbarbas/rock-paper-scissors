f2ed2953ca96483a37007707540d3c074d6f9dbd
Christopher Barbas <chrisbarbas30@gmail.com>
2022-01-06 16:04:49 -0500

    rock paper scissors using the console log, first to 5 wins

---
+++ script.js
@@ -0,0 +1,59 @@
+//rolls for computer
+function computerPlay() {
+    let random = Math.floor(Math.random() * 3) + 1;
+    if (random === 1) {
+        return 'rock';
+    } else if (random === 2) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+//compares computers roll with player's answer, increases score
+function playRound(playerSelection, computerSelection) {
+    console.log(`Player selected ${playerSelection}`);
+    console.log(`Computer selected ${computerSelection}`);
+    if (playerSelection == computerSelection) {
+        console.log("It's a tie")
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        console.log('You Win! Rock beats Scissors')
+        playerScore++;
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        console.log('You Lose! Paper beats Rock')
+        computerScore++;
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        console.log('You Win! Paper beats Rock')
+        playerScore++;
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        console.log('You Lose! Scissors beats Paper')
+        computerScore++;
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        console.log('You Win! Scissors beats Paper')
+        playerScore++;
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        console.log('You Lose! Rock beats Scissors')
+        computerScore++;
+    }
+}
+
+//runs playRound until first to 5 + prompts user
+function game() {
+    while (playerScore < 5 && computerScore < 5) {
+        const playerSelection = prompt("Pick a move").toLowerCase();
+        const computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+        console.log(`Player's score: ${playerScore}`);
+        console.log(`Computer's score: ${computerScore}`);
+        //first to 5 wins
+        if (playerScore === 5) {
+            console.log('PLAYER WON!!!')
+        } else if (computerScore === 5) {
+            console.log('COMPUTER WON!!!')
+        }
+    }
+}
+
+let playerScore = 0;
+let computerScore = 0;
+game();
